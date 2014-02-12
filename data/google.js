ddac44c1bfb4e104b1f214dc9a36f576c07d0d31
MorbZ <morbz1@gmail.com>
2014-02-12 16:15:50 +0100

    Don't show "anonym" label instead of country name when there is no country name

--- data/google.js
+++ data/google.js
@@ -2,9 +2,7 @@
 var logo = document.getElementById("hplogo");
 if(logo != undefined) {
 	var anonymous = "anonym";
-	if(logo.hasChildNodes) {
+	if(logo.hasChildNodes()) {
 		logo.firstChild.innerHTML = anonymous;
-	} else {
-		logo.innerHTML = '<div nowrap="" style="color:#777;font-size:16px;font-weight:bold;position:relative;left:218px;top:70px">' + anonymous + '</div>'
 	}
 }
