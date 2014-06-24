ac8e5371b185606bba567700b091ee36846a7792
MorbZ <morbz1@gmail.com>
2014-06-25 00:50:03 +0200

    Fix doodle problem and display anonym label/link in bottom bar
    Fixes #5
    Fixes #6

--- firefox/data/google.js
+++ firefox/data/google.js
@@ -1,8 +1,22 @@
 //replace country name with own text
 var logo = document.getElementById("hplogo");
 if(logo != undefined) {
-	var anonymous = "anonym";
-	if(logo.hasChildNodes()) {
-		logo.firstChild.innerHTML = anonymous;
+	//non-US version?
+	if(logo.hasChildNodes() && logo.nodeName == "DIV") {
+		//check for doodle
+		var logoChild = logo.firstChild;
+		if(logoChild.hasChildNodes()) {
+			//only text?
+			var textChild = logoChild.firstChild;
+			if(textChild.nodeValue) {
+				textChild.nodeValue = "anonym";
+			}
+		}
 	}
+}
+
+//add link to bottom bar
+var bar = document.getElementById("fsl");
+if(bar != undefined) {
+	bar.innerHTML += '<a style="font-weight: bold;" class="_Ld" target="_blank" href="https://addons.mozilla.org/firefox/addon/searchonymous/">anonym &#10003;</a>';
 }
