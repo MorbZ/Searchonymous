504bcdddd34657a8f7c748d9f07e71c9ceefe3dc
Merten Peetz <dev@morbz.net>
2019-02-24 00:45:19 +0100

    Avoid unsafe innerHTML by using Markdown links

--- src/options.js
+++ src/options.js
@@ -34,29 +34,53 @@ function initOptions() {
 	});
 }
 
+/* Localization */
 function localize() {
 	for(let loc of [
-		// [selector, message name, ([placeholders])]
+		// [selector, message name]
 		['p.header', 'optionsHeader'],
 		['span.allow', 'optionsAllow'],
 		['span.block', 'optionsBlock'],
 		['div.preferencesCookie > h2', 'optionsPreferencesCookie'],
-		['div.preferencesCookie > p', 'optionsPreferencesCookieDescription', [
-			'https://www.google.com/policies/technologies/types/',
-		]],
+		['div.preferencesCookie > p', 'optionsPreferencesCookieDescription'],
 		['div.consentCookie > h2', 'optionsConsentCookie'],
 		['div.consentCookie > p', 'optionsConsentCookieDescription'],
 	]) {
 		// Get localized string
-		let params = [];
-		if(loc[2] !== undefined) {
-			params = loc[2];
-		}
-		let message = chrome.i18n.getMessage(loc[1], params);
+		let message = chrome.i18n.getMessage(loc[1]);
 
 		// Apply string to elements
 		for(let elem of document.querySelectorAll(loc[0])) {
-			elem.innerHTML = message;
+			appendLinkText(elem, message);
 		}
+	}
+}
+
+// Avoid unsafe assignment to innerHTML by manually creating DOM elements.
+// Uses Markdown syntax for replacing "[name](url)" with a HTML link.
+function appendLinkText(elem, str) {
+	// Regex: https://regex101.com/r/214K89/2
+	let re = /\[(.*?)\]\((.*?)\)/g;
+	let i = 0;
+	let m;
+	while((m = re.exec(str)) != null) {
+		// Add text
+		appendText(elem, str.substr(i, m.index-i));
+		i = m.index + m[0].length;
+
+		// Add link
+		let a = document.createElement('a');
+		a.href = m[2];
+		appendText(a, m[1]);
+		elem.appendChild(a);
+	}
+
+	// Add remaining text
+	appendText(elem, str.substr(i));
+}
+
+function appendText(elem, str) {
+	if(str.length > 0) {
+		elem.appendChild(document.createTextNode(str));
 	}
 }
